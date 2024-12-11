87c786acaf6fbe9d739af4e43173a39b045cc055
senalbulumulle <senal.bulumulle@gmail.com>
2024-12-11 18:34:29 -0500

    I take it back

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import './App.css'
 import './position-text.css'
 import { Link } from 'react-router-dom';
 // import './UI.css'
-// import { BrowserRouter as Router, Route, Routes, Link} from "react-router-dom";
 
 
 function App() {
@@ -13,6 +12,38 @@ function App() {
 
     return (
         <>
+            <div class="layout indicator-side-round">
+                <h1> Subjects </h1>
+                <a href="http://127.0.0.1:3000" class="button small button is-red button-no-animate-mode">Back</a>
+            </div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+
+
+            <div class="layout side-round">
+                <h1> List</h1>
+
+                <button class="button">3D Sphere Test</button>
+            </div>
         </>
     )
 }
