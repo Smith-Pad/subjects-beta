ef2db8e21651f5ce49c9d594eab447f9125ebf0b
senalbulumulle <senal.bulumulle@gmail.com>
2024-12-11 11:19:20 -0500

    created a button in the subjects beta screen called 3D-Sphere Test

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import './position-text.css'
+// import './UI.css'
+
 
 function App() {
     const [count, setCount] = useState(0)
@@ -10,9 +13,38 @@ function App() {
         <>
             <div class="layout indicator-side-round">
                 <h1> Subjects </h1>
+                <a href="http://127.0.0.1:3000" class="button small button is-red button-no-animate-mode">Back</a>
+            </div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+
+
+            <div class="layout side-round">
+                <h1> List</h1>
+
+                <button class="button">3D Sphere Test</button>
             </div>
         </>
     )
 }
 
-export default App  
+export default App
