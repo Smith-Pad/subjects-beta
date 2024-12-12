ee2bf2f3c79666fe5f2cfdb99ca4b5c36bd4f79f
senalbulumulle <senal.bulumulle@gmail.com>
2024-12-11 20:01:04 -0500

    oh.. that might work...

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import './position-text.css'
-
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
     const [count, setCount] = useState(0)
 
     return (
         <>
-
+            hello world
         </>
     )
 }
