ad380d9c164b05da9868836bbbb6e360032bb9e6
senalbulumulle <senal.bulumulle@gmail.com>
2024-12-12 03:57:22 -0500

    oh okay

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,44 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import './position-text.css'
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
     const [count, setCount] = useState(0)
 
     return (
         <>
-            hello world
+            <div class="layout indicator-side-round">
+                <h1> Subjects </h1>
+                <a href="http://127.0.0.1:3000" class="button small button is-red button-no-animate-mode">Back</a>
+            </div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+
+
+            <div class="layout side-round">
+                <h1> List</h1>
+
+                <button class="button">3D Sphere Test</button>
+            </div>
         </>
     )
 }
