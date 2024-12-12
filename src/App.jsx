df05d42705036ed44361d890c028eacfaca500ac
senalbulumulle <senal.bulumulle@gmail.com>
2024-12-11 19:47:23 -0500

    oh?

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import './position-text.css'
-// import './UI.css'
-
+import { Link } from 'react-router-dom';
 
 function App() {
     const [count, setCount] = useState(0)
