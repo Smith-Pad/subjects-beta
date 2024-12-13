9f59bd897933a6a88142440a82d94f5d91b67a85
senalbulumulle <senal.bulumulle@gmail.com>
2024-12-13 12:24:29 -0500

    Try to at least to organize.... senal

---
+++ JSX-About-Subjects/index.jsx
@@ -0,0 +1,30 @@
+function About() {
+
+    const Navigate = useNavigate();
+
+    const routeToHome = () => {
+        Navigate('/');
+    };
+
+    return (
+        <>
+            <div class="layout indicator-side-round">
+                <h1> About </h1> <button class="button small button button-no-animate-mode" onClick={routeToHome}>Back</button>
+            </div>
+
+
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+        </>
+    );
+}
---
+++ JSX-Home-Screen/index.jsx
@@ -0,0 +1,45 @@
+function Home() {
+
+    const Navigate = useNavigate();
+
+    const routeToAbout = () => {
+        Navigate('/about');
+    };
+
+    return (
+        <>
+            <div class="layout indicator-side-round">
+                <h1> Subjects </h1>
+                <a href="http://127.0.0.1:3000" class="button small button is-red button-no-animate-mode">Back</a>
+            </div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+            <div class="space"></div>
+
+
+            <div class="layout side-round">
+                <h1> List</h1>
+
+                <button class="button small button button-no-animate-mode" onClick={routeToAbout}>Go to About</button>
+            </div>
+        </>
+    );
+}
