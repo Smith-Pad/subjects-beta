af53017a268f9974c23859fe4554e074681c6151
senalbulumulle <senal.bulumulle@gmail.com>
2024-12-19 13:12:23 -0500

    removed the react router stuff and everything

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 // Group 1: React imports
 import { useState } from 'react';
-import { BrowserRouter, Routes, Route, Link, useNavigate } from 'react-router-dom'; // Import useNavigate here
 
 
 
@@ -9,97 +8,11 @@ import './App.css';
 import './position-text.css';
 
 
-function Home() {
-
-    const Navigate = useNavigate();
-
-    const routeToAbout = () => {
-        Navigate('/about');
-    };
-
-    return (
-        <>
-            <div class="layout indicator-side-round">
-                <h1> Subjects </h1>
-                <a href="http://127.0.0.1:3000" class="button small button is-red button-no-animate-mode">Back</a>
-            </div>
-            <div class="space"></div>
-            <div class="space"></div>
-            <div class="space"></div>
-            <div class="space"></div>
-            <div class="space"></div>
-            <div class="space"></div>
-            <div class="space"></div>
-            <div class="space"></div>
-            <div class="space"></div>
-            <div class="space"></div>
-            <div class="space"></div>
-            <div class="space"></div>
-            <div class="space"></div>
-            <div class="space"></div>
-            <div class="space"></div>
-            <div class="space"></div>
-            <div class="space"></div>
-            <div class="space"></div>
-            <div class="space"></div>
-            <div class="space"></div>
-            <div class="space"></div>
-
-
-            <div class="layout side-round">
-                <h1> List</h1>
-
-                <button class="button small button button-no-animate-mode" onClick={routeToAbout}>About Subjects</button>
-            </div>
-        </>
-    );
-}
-
-
-
-
-function About() {
-
-    const Navigate = useNavigate();
-
-    const routeToHome = () => {
-        Navigate('/');
-    };
-
-    return (
-        <>
-            <div class="layout indicator-side-round">
-                <h1> About </h1> <button class="button small button button-no-animate-mode" onClick={routeToHome}>Back</button>
-            </div>
-
-
-            <div class="space"></div>
-            <div class="space"></div>
-            <div class="space"></div>
-            <div class="space"></div>
-            <div class="space"></div>
-            <div class="space"></div>
-            <div class="space"></div>
-            <div class="space"></div>
-            <div class="space"></div>
-            <div class="space"></div>
-            <div class="space"></div>
-            <div class="space"></div>
-        </>
-    );
-}
-
-
-
-
 function App() {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-            </Routes>
-        </BrowserRouter>
+        <>
+        
+        </>
     );
 }
 
